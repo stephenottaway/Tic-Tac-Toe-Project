b0de89ca89dd622873b250356a3f584c70f2287a
stephenottaway <stephenott14@icloud.com>
2024-03-11 21:24:03 -0700

    Completed checkForThreeInARow

--- tictactoe.c
+++ tictactoe.c
@@ -6,7 +6,7 @@
 
 /*TO DO: Add function to check if game is over, get rid of initialization functions (will find solution to those later), add function to modify string for board printing, and add function to check if a tile is empty */
 
-char board[] = "___|___|___\n___|___|___\n   |   |   \n";
+char board[] = "_a[0]__|_a[1]__|_a[2]__\n_a[3]__|_a[4]__|_a[5]__\n a[6]  | a[7]  | a[8]  \n";
 
 
 typedef struct {
@@ -28,8 +28,20 @@ int checkForFullBoard(int* gameState, size_t size) {
 	printf("Error: size of array must be 9\n");
 	return 0;
     }
+    // May store as a variable later to make the code a bit cleaner
     return gameState[0] && gameState[1] && gameState[2] && gameState[3] && gameState[4] && gameState[5] && gameState[6] && gameState[7] && gameState[8]; 
 } 
+
+int checkForThreeInARow(int* board, size_t size) {
+    if(size != BOARD_SIZE) {
+	printf("Error: size of array must be 9\n");
+	return 0;
+    }
+    // TO DO: Add all possible combinations of three in a row that would end the game (may store as ints later to clean up the code, separating into three in a row on the rows, the columns, and along the diagonals)
+    return ((board[0] && board[1] && board[2]) || (board[3] && board[4] && board[5]) || (board[6] && board[7] && board[8]) || (board[0] && board[3] && board[6]) || (board[1] && board[4] && board[7]) || (board[2] && board[5] && board[8])
+    || (board[0] && board[4] && board[8]) || (board[2] && board[4] && board[6]));
+}
+
 
 // Comment out unused/incorrect methods for now
 /*void displayBoard(char* board) {
